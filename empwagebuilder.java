ce172ed5ecbef96225fe8d5c4ddfae48e8b4af01
manikiranp <manikiranp3@gmail.com>
2020-10-18 16:40:40 +0530

    checking employee presence

--- empwagebuilder.java
+++ empwagebuilder.java
@@ -1,7 +1,14 @@
-
+import java.util.Random;
 public class empwagebuilder {
 
         public static void main(String[] args) {
 	System.out.println("Welcome to Employee wage builder program in Java");
+	int full_time=1;
+	Random randnum = new Random(); //random class instance
+	int empcheck = randnum.nextInt(2);
+	if(empcheck==full_time)
+		System.out.println("Employee is Present");
+	else
+		System.out.println("Employee is absent");
 	}
 }
