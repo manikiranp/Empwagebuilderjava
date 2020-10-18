f7ef08ad4bb721137c434b7e60954685b7d70938
manikiranp <manikiranp3@gmail.com>
2020-10-18 23:10:03 +0530

    Calculating employee daily wage

--- empwagebuilder.java
+++ empwagebuilder.java
@@ -1,14 +1,20 @@
+//employee daily wage calculator
 import java.util.Random;
 public class empwagebuilder {
 
-        public static void main(String[] args) {
-	System.out.println("Welcome to Employee wage builder program in Java");
-	int full_time=1;
+	public static void main(String[] args) {
+	int full_time=1; int emp_rate_per_hr=20; //constants
+	int emphrs=0; int empwage=0;	//variables
 	Random randnum = new Random(); //random class instance
 	int empcheck = randnum.nextInt(2);
-	if(empcheck==full_time)
+	
+	if (empcheck == full_time) {
 		System.out.println("Employee is Present");
-	else
+		emphrs=8; }
+	else {
 		System.out.println("Employee is absent");
+		emphrs=0; }
+	empwage = emphrs*emp_rate_per_hr;
+	System.out.println("Employee daily wage: " + empwage);
 	}
 }
