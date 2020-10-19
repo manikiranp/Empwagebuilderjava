cddf45d1e4401904d7ab749c5bb81550f6a5e7cd
manikiranp <manikiranp3@gmail.com>
2020-10-19 12:12:58 +0530

    Added part-time in daily wage calculation

--- empwagebuilder.java
+++ empwagebuilder.java
@@ -1,16 +1,22 @@
-//employee daily wage calculator
+//employee part time
 import java.util.Random;
 public class empwagebuilder {
 
 	public static void main(String[] args) {
-	int full_time=1; int emp_rate_per_hr=20; //constants
-	int emphrs=0; int empwage=0;	//variables
+	int full_time=2;	//constants
+	int part_time=1;
+	int emp_rate_per_hr=20;
+	int emphrs=0;	//variables
+	int empwage=0;
 	Random randnum = new Random(); //random class instance
-	int empcheck = randnum.nextInt(2);
+	int empcheck = randnum.nextInt(3);
 	
 	if (empcheck == full_time) {
 		System.out.println("Employee is Present");
 		emphrs=8; }
+	else if (empcheck == part_time) {
+		System.out.println("Employee is present part-time");
+		emphrs=4; }
 	else {
 		System.out.println("Employee is absent");
 		emphrs=0; }
