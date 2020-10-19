27f261a4204a8481a60a30dab49be8748974fe88
manikiranp <manikiranp3@gmail.com>
2020-10-19 15:43:21 +0530

    monthly wage calculation based on given conditions

--- empwagebuilder.java
+++ empwagebuilder.java
@@ -5,11 +5,12 @@ public class empwagebuilder {
 	public static final int part_time=1;
 	public static final int emp_rate_per_hr=20;
 	public static final int num_work_days=20;
+	public static final int max_hrs_in_month=100;
 public static void main(String[] args) {
 	//variables
-	int emphrs=0, empwage=0, totalempwage=0;
+	int emphrs=0, empwage=0, totalempwage=0, totalemphrs=0, totalworkdays=0;
 	Random randnum = new Random(); //random class instance
-	for (int day=1; day<=num_work_days; day++) {
+	while (totalemphrs <= max_hrs_in_month && totalworkdays <= num_work_days) {
 	int empcheck = randnum.nextInt(3);
 	switch (empcheck) {
 		case full_time:
@@ -20,10 +21,13 @@ public static void main(String[] args) {
 		break;
 		default:
 		emphrs=0; }
+	totalemphrs += emphrs;
 	empwage = emphrs*emp_rate_per_hr;
 	totalempwage += empwage;
-	System.out.println("Employee day"+day+" wage: " + empwage);
-		}
+	System.out.println("Employee day"+totalworkdays+" wage: " + empwage);
+	totalworkdays++;
+	 }
+	System.out.println("\nTotal working hours: " + totalemphrs );
 	System.out.println("Total employee monthly wage: " + totalempwage);
 	}
 }
