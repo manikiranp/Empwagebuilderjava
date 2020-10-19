6cb200d87847d0f31615584c0206ab71a425c203
manikiranp <manikiranp3@gmail.com>
2020-10-19 12:50:37 +0530

    solving employee wage using switch-case

--- empwagebuilder.java
+++ empwagebuilder.java
@@ -1,23 +1,24 @@
 //employee part time
 import java.util.Random;
 public class empwagebuilder {
-
-	public static void main(String[] args) {
-	int full_time=2;	//constants
-	int part_time=1;
-	int emp_rate_per_hr=20;
+	public static final int full_time=2;
+	public static final int part_time=1;
+	public static final int emp_rate_per_hr=20;
+public static void main(String[] args) {
 	int emphrs=0;	//variables
 	int empwage=0;
 	Random randnum = new Random(); //random class instance
 	int empcheck = randnum.nextInt(3);
-	
-	if (empcheck == full_time) {
+	switch (empcheck) {
+		case full_time:
 		System.out.println("Employee is Present");
-		emphrs=8; }
-	else if (empcheck == part_time) {
+		emphrs=8;
+		break;
+		case part_time:
 		System.out.println("Employee is present part-time");
-		emphrs=4; }
-	else {
+		emphrs=4;
+		break;
+		default:
 		System.out.println("Employee is absent");
 		emphrs=0; }
 	empwage = emphrs*emp_rate_per_hr;
