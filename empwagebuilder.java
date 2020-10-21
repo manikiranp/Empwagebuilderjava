00c7ed124cbd88854e3a515e24713d4c1349c2fb
manikiranp <manikiranp3@gmail.com>
2020-10-21 10:23:13 +0530

    change in workingdays conditions

--- empwagebuilder.java
+++ empwagebuilder.java
@@ -1,4 +1,4 @@
-//employee part time
+
 import java.util.Random;
 public class empwagebuilder {
 	public static final int full_time=2;
@@ -10,22 +10,26 @@ public static void main(String[] args) {
 	//variables
 	int emphrs=0, empwage=0, totalempwage=0, totalemphrs=0, totalworkdays=0;
 	Random randnum = new Random(); //random class instance
-	while (totalemphrs <= max_hrs_in_month && totalworkdays <= num_work_days) {
+	while (totalemphrs <= max_hrs_in_month && totalworkdays < num_work_days) {
 	int empcheck = randnum.nextInt(3);
+	totalworkdays++;
 	switch (empcheck) {
 		case full_time:
 		emphrs=8;
+		System.out.println("Employee is present fulltime:");
 		break;
 		case part_time:
 		emphrs=4;
+		System.out.println("Employee is present part-time:");
 		break;
 		default:
-		emphrs=0; }
+		emphrs=0;
+		System.out.println("Employee is Absent:");
+			 }
 	totalemphrs += emphrs;
 	empwage = emphrs*emp_rate_per_hr;
 	totalempwage += empwage;
-	System.out.println("Employee day"+totalworkdays+" wage: " + empwage);
-	totalworkdays++;
+	System.out.println("Day"+totalworkdays+" wage: " + empwage);
 	 }
 	System.out.println("\nTotal working hours: " + totalemphrs );
 	System.out.println("Total employee monthly wage: " + totalempwage);
